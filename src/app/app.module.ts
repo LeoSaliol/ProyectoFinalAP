611f748257c1b8dcb0498222f7129670f91189ec
leonel <leonelsaliol@hotmail.com>
2022-05-08 18:46:36 -0300

    Primero commit Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { BodyComponent } from './componentes/body/body.component';
+import { HeaderComponent } from './componentes/body/compBody/header/header.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'inicio', component: BodyComponent }, 
+  { path: '', component: BodyComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    BodyComponent,
+    HeaderComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
