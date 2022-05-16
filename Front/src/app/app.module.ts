0ea0ac9adace7e08b8fc593ca4048176a167e219
leonel <leonelsaliol@hotmail.com>
2022-05-16 17:01:16 -0300

    carpetas

---
+++ Front/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './componentes/nav/nav.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { BodyComponent } from './componentes/body/body.component';
+import { HeaderComponent } from './componentes/body/compBody/header/header.component';
+import { ExperienciaComponent } from './componentes/body/compBody/experiencia/experiencia.component';
+import { AcercaDeComponent } from './componentes/body/compBody/acerca-de/acerca-de.component';
+import { EducacionComponent } from './componentes/body/compBody/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardySkillComponent } from './componentes/body/compBody/hardy-skill/hardy-skill.component';
+import { FooterComponent } from './componentes/body/compBody/footer/footer.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'inicio', component: BodyComponent }, 
+  { path: '', component: BodyComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    LoginComponent,
+    BodyComponent,
+    HeaderComponent,
+    ExperienciaComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    HardySkillComponent,
+    FooterComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    NgCircleProgressModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
